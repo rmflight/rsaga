8c92568a97e33a4f430dc9073494d3475cb7e85f
Robert Flight <r.flight79@gmail.com>
2014-03-13 10:50:22 -0400

    renaming a few variable names to make it easier to follow

--- R/sa_stuff.r
+++ R/sa_stuff.r
@@ -1,20 +1,20 @@
 #' run simulated annealing
 #'
 #' @param initialSolution a starting solution
-#' @param evalFunction function to evaluate each iteration of the solution
+#' @param energyFunction function to evaluate each iteration of the solution
 #' @param neighborFunction the function to create a new solution from the current one
 #' @param initialTemperature the starting temperature (default is 300)
 #' @param deltaTemperature how much to decrease the temperature at each step (default is 1)
 #' @param temperatureStep how many steps to go from \code{initialTemperature} to 0 (default is NA), overrides \code{deltaTemperature}
 #' @param alpha how much the current temperature affects the neighbor solution
-#' @param sameTolerance how similar two iterations have to be to stop
+#' @param stopTolerance how similar two iterations have to be to stop
 #' @param nTry how many overall iterations to do
 #' @param tryGood how many good solutions to generate before reducing temperature (default is 10)
 #' @param tryBad how many bad solutions to make before reducing temperature (default is 100)
 #' 
 #' @export
 #' @return list of results
-sa <- function(initialSolution, evalFunction, neighborFunction, initialTemperature=300, deltaTemperature=1, temperatureStep=NA, alpha=1, sameTolerance=0.00001, nTry=10000, tryGood=10, tryBad=100){
+sa <- function(initialSolution, energyFunction, neighborFunction, initialTemperature=300, deltaTemperature=1, temperatureStep=NA, alpha=1, stopTolerance=0.00001, nTry=10000, tryGood=10, tryBad=100){
   
   if (!is.na(temperatureStep)){
     deltaTemperature <- (initialTemperature - 0) / temperatureStep
@@ -34,7 +34,7 @@ sa <- function(initialSolution, evalFunction, neighborFunction, initialTemperatu
   allSolutions <- vector("list", nIter)
   allEnergy <- vector("double", nIter)
   
-  while ((currTemp > 0) && (currTol > sameTolerance) && (iTry < nTry)){
+  while ((currTemp > 0) && (currTol > stopTolerance) && (iTry < nTry)){
     temperatureTry <- 1
     iGood <- 0
     iBad <- 0
@@ -44,7 +44,7 @@ sa <- function(initialSolution, evalFunction, neighborFunction, initialTemperatu
     tmpNewEnergy <- vector("double", 10)
     while ((iGood < tryGood) && (iBad < tryBad)){
       newSolution <- neighborFunction(currentPopulation=currSolution, currentTemperature=currTemp, alpha=alpha)
-      newEnergy <- evalFunction(newSolution)
+      newEnergy <- energyFunction(newSolution)
       
       deltaEnergy <- newEnergy - currEnergy
       
